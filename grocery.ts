b2c18822f306c911b13670c5a33ba77dbabb5496
carolinenewcom <132967202+carolinenewcom@users.noreply.github.com>
2023-10-29 16:09:46 -0500

    Add files via upload

---
+++ grocery.ts
@@ -0,0 +1,29 @@
+class Grocery {
+    name: string;
+    quantity: number;
+    total:number;
+
+    constructor(name: string, quantity: number, total: number) {
+        this.name = name;
+        this.quantity = quantity;
+        this.total = total;
+    }
+}
+
+let grocery_list = [
+    new Grocery('Milk', 3, 11.85),
+    new Grocery('Bread', 6, 15.18),
+    new Grocery('Eggs', 11, 27.17),
+    new Grocery('Apples', 5, 6.95),
+    new Grocery('Paper Towels', 1, 6.99),
+    new Grocery('Soap', 12, 9.49),
+    new Grocery('Coffee', 2, 13.98),
+    new Grocery('Batteries', 1, 17.87)
+]
+
+const elem = document.getElementById('list');
+grocery_list.forEach(product => {
+    const p = document.createElement("p");
+    p.textContent = `${product.name} (${product.quantity}) $${product.total}`;
+    elem.appendChild(p);
+});
