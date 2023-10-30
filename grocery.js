cc42c60efbf69b2a1facb823ff34e91840cf2b43
carolinenewcom <132967202+carolinenewcom@users.noreply.github.com>
2023-10-29 20:38:30 -0500

    Add files via upload

---
+++ grocery.js
@@ -0,0 +1,18 @@
+var Grocery = /** @class */ (function () {
+    function Grocery(name, quantity, total) {
+        this.name = name;
+        this.quantity = quantity;
+        this.total = total;
+    }
+    return Grocery;
+}());
+var grocery_list = [
+    new Grocery('Milk', 3, 11.85),
+    new Grocery('Bread', 6, 15.18),
+    new Grocery('Eggs', 11, 27.17),
+    new Grocery('Apples', 5, 6.95),
+    new Grocery('Paper Towels', 1, 6.99),
+    new Grocery('Soap', 12, 9.49),
+    new Grocery('Coffee', 2, 13.98),
+    new Grocery('Batteries', 1, 17.87)
+];
